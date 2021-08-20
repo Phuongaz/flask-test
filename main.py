5f160085f0e155f7e5094d5abe26e448d540be18
Nguyen Tan Phuong <37770098+Phuongaz@users.noreply.github.com>
2021-08-20 13:08:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for, request, redirect
+from query import status
+import asyncio
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'test' or request.form['password'] != 'test':
+            error = 'Login attempt failed! Your username or password is incorrect!'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/home')
+def home():
+    result = None
+    port = 19132
+    if request.method == 'POST':
+        ipaddress = request.form['host']
+        port = int(request.form['port'])
+        if ipaddress != "":
+            result = asyncio.run(status(ipaddress, port))
+            print(result)
+
+    return render_template("home.html", result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
